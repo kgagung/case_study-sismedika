e0002b4a82efe584dd0413416639b86f43c16b5b
kuga24 <galihkuncoro19@gmail.com>
2025-05-20 15:13:02 +0700

    add log text

--- middleware/logger.go
+++ middleware/logger.go
@@ -3,18 +3,30 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 // LoggerMiddleware adalah middleware untuk mencatat setiap permintaan yang masuk
 func LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Buka file log (append mode)
+		f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Println("Gagal membuka log.txt:", err)
+			return
+		}
+		defer f.Close()
+
+		// Set log output ke file
+		log.SetOutput(f)
+
 		start := time.Now()
 
-		// Menjalankan handler berikutnya
+		// Jalankan handler berikutnya
 		next.ServeHTTP(w, r)
 
-		// Mencatat informasi permintaan
+		// Tulis log ke file
 		log.Printf(
 			"%s %s %s %s",
 			r.Method,
